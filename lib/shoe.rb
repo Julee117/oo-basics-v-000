28078ec4a7c84a4754a3390c54a3debd93d7b958
Julianne Kim <juliannne@gmail.com>
2017-10-20 13:26:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,5 @@
 class Shoe
-
+  attr_accessor :color, :size, :material :condition
   def initialize(brand)
     @brand = brand
   end
@@ -8,29 +8,6 @@ class Shoe
     @brand
   end
 
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size = size
-  end
-
-  def size
-    @size
-  end
-
-  def material=(material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
 
   def condition=(condition)
     @condition = condition
