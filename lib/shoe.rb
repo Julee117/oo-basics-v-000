988ad33fc172d09bff3eb00f062e79dfb52d990b
Julianne Kim <juliannne@gmail.com>
2017-10-20 13:27:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,15 +8,6 @@ class Shoe
     @brand
   end
 
-
-  def condition=(condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
-  end
-
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
